c5beff0f588710ea43231355401666ec4e18003a
vine <vinicius.dutra@gmail.com>
2022-05-05 11:38:33 -0300

    Sent Success screen

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -4,6 +4,7 @@ import thoughtImageUrl from '../../assets/thought.svg'
 import { useState } from "react";
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 import { FeedbackContentStep } from './Steps/FeedbackContentStep';
+import { FeedbackSuccessStep } from './Steps/FeedbackSuccessStep';
 
 export const feedbackTypes = {
     BUG: {
@@ -32,23 +33,32 @@ export const feedbackTypes = {
 export type FeedbackType = keyof typeof feedbackTypes
 
 export function WidgetForm() {
-	const [FeedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+	const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+	const [feedbackSent, setFeedbackSent] = useState(false)
+
 	function handleRestartFeedback(){
+		setFeedbackSent(false)
 		setFeedbackType(null)
 	}
 
 	return (
 		<div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
 			
-			{ !FeedbackType ? (
-				<FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+			{ feedbackSent ? (
+				<FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback} />
 			) : (
-				<FeedbackContentStep
-					feedbackType={FeedbackType}
-					onFeedbackRestartRequested={handleRestartFeedback}
-				/>
-			)
-			}
+				<>
+					{ !feedbackType ? (
+						<FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+					) : (
+						<FeedbackContentStep
+							feedbackType={feedbackType}
+							onFeedbackRestartRequested={handleRestartFeedback}
+							onFeedbackSent={()=>setFeedbackSent(true)}
+						/>
+					)}
+				</>
+			) }
 
 			<footer className="text-xs text-neutral-400">
 				Made with ♥ by vine
