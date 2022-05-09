a31f99a2b3f9ccfc2b81c08dc0a01bc5ad91ec6e
vine <vinicius.dutra@gmail.com>
2022-05-09 11:58:49 -0300

    Adjusted image size at email body

--- server/src/use-cases/submit-feedback-use-case.ts
+++ server/src/use-cases/submit-feedback-use-case.ts
@@ -39,7 +39,7 @@ export class SubmitFeedbackUseCase {
         `<div style="font-family: sans-serif; font-size: 16px; color: #111" >`,
         `<p>Feedback type: ${type}</p>`,
         `<p>Comment: ${comment}</p>`,
-        screenshot ? `<img src="${screenshot}" />` : null,
+        screenshot ? `<img src="${screenshot}" style="width:800;height:auto;" />` : null,
         `</div>`
       ].join('\n')
     })
